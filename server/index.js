cf2391b5a000ec7038ce914a8981b5cdd828bc4b
Bekzhan Shaikhanzar Galymzhan <bekzhanshaikhanzar@MacBook-Air-Bekzhan.local>
2026-03-23 00:05:23 +0500

    🔄 Update server config for deployment

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 const express = require('express');
+const cors = require('cors');
 const helmet = require('helmet');
 const morgan = require('morgan');
 const { getDatabase, getOne, getAll, runQuery } = require('./db/database');
